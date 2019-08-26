7a26a50502a8b51a0bc8af6271004ae1be22a99d
Taisei <jinhorror606@gmail.com>
2019-08-26 16:35:42 +0300

    init routes 5.8

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,35 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function() {
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+//> Админка Блога
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix'    => 'admin/blog',
+];
+
+Route::group($groupData, function () {
+   // BlogCategory
+    $methods = ['index', 'edit', 'update', 'create', 'store',];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+
+    //BlogPost
+    Route::resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
+});
+//<
+
+
+
+
+//Route::resource('rest', 'RestTestController')->names('restTest');
+
